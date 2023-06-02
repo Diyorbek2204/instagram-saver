15a48a45fa89b4b0792a592d0333b1b7948eaec8
Diyorbek2204 <diyorbekabdimalikov88@gmail.com>
2023-06-02 14:02:46 +0500

    Added bot.conf

--- data/config.py
+++ data/config.py
@@ -4,6 +4,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")
-ADMINS = env.list("ADMINS")
-IP = env.str("IP")
+BOT_TOKEN = "5995521931:AAEtf4bDyAQ6c-7YKFWIaK6WZgIL4iAgC-k"
+ADMINS = [1824528502,]
+IP = "localhost"
--- keyboards/inline/inline_buttons.py
+++ keyboards/inline/inline_buttons.py
@@ -5,9 +5,7 @@ def inline_button_option():
     ikm_option = InlineKeyboardMarkup()
 
     button_option = InlineKeyboardButton(text="🟣Instagram", callback_data="instagram")
-    button_option2 = InlineKeyboardButton(text="🔴YouTube", callback_data="youtube")
-    button_option3 = InlineKeyboardButton(text="⚫TikTok", callback_data="tiktok")
 
-    ikm_option.add(button_option, button_option2, button_option3)
+    ikm_option.add(button_option)
 
     return ikm_option
